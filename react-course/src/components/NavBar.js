f49082837896e0d0bcca4e9299d4802f60f6a737
Nicky <nickysauceofficial@gmail.com>
2023-01-21 17:07:31 +0530

    tenzie

--- react-course/src/components/NavBar.js
+++ react-course/src/components/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = ({ props }) => {
   return (
     <nav className={props.darkMode ? "dark" : ""}>
       <img className="nav--logo_icon" src="" alt="" />
-      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h3 className="nav--logo_text">React Facts</h3>
 
       <div className="toggler">
         <p className="toggler--light">Light</p>
